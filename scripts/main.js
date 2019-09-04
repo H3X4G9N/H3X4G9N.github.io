61727f2e5e7001e21eaac1f610dad3094d3ed128
H3X4G9N <30376065+H3X4G9N@users.noreply.github.com>
2019-09-04 17:29:02 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,11 @@
+/**/
+let backgroundColor = [0, 0, 0, 1];
+let zoom = 100;
+let zoomSpeed = 1;
+let cameraPosition = [0, 0];
+let cameraSpeed = 1;
+/**/
 
+let rectangle1 = new Rectangle([0, 0], 0, [1, 1], [255, 255, 255]);
+
+render();
