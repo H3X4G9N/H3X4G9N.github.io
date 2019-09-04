fbc8ca89af4a4c03c1b524523c4abc6e8638a64b
H3X4G9N <30376065+H3X4G9N@users.noreply.github.com>
2019-09-04 17:40:32 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,11 @@
+function randomIn(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomColor() {
+    return [randomIn(0, 255), randomIn(0, 255), randomIn(0, 255)];
+}
+
 /**/
 let backgroundColor = [0, 0, 0, 1];
 let zoom = 100;
@@ -7,5 +15,9 @@ let cameraSpeed = 1;
 /**/
 
 let rectangle1 = new Rectangle([0, 0], 0, [1, 1], [255, 255, 255]);
+
+for (let a = 0; a < 100; a++) {
+    new Rectangle([randomIn(-100, 100), randomIn(-100, 100)], randomIn(0, 360), [randomIn(0, 10), randomIn(0, 10)], randomColor());
+}
 
 render();
