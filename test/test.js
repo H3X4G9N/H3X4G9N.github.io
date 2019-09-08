b22d2f6a85cbcf0dc22556c6411faa9e97303947
H3X4G9N <30376065+H3X4G9N@users.noreply.github.com>
2019-09-08 17:17:20 +0300

    Update test.js

--- test/test.js
+++ test/test.js
@@ -4,6 +4,6 @@ document.body.appendChild(app.view);
 var graphics = new PIXI.Graphics();
 app.stage.addChild(graphics);
 
-graphics.beginFill();
+graphics.beginFill(0xff0000);
 graphics.drawRect(0, 0, 300, 200);
 graphics.angle = 10;
